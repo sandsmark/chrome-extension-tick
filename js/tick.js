16d46eec4d067706a85ac78a7cd0fb33317a0666
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-11-26 18:36:01 +0100

    fix 'fancy' sites

--- js/tick.js
+++ js/tick.js
@@ -3,9 +3,11 @@
 function DOMtoString() {
     var checkedBoxes = document.getElementsByTagName('input');
     for(var i = 0; i < checkedBoxes.length; i++){
-        if(checkedBoxes[i].type=='checkbox'){
-          checkedBoxes[i].checked = true;
-        }
+            if (checkedBoxes[i].checked === false) {
+                checkedBoxes[i].click();
+            } else {
+                checkedBoxes[i].checked = true;
+            }
     }
 }
 
